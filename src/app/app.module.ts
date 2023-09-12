17232b735a095f667e7e30979f23ea775f733e7c
gehad-abdelmonem <gehadabdelmonam@gmail.com>
2023-09-12 13:22:01 +0300

    Adding loading interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
@@ -6,15 +5,19 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './component/shared/shared.module';
 import { ErrorInterceptor } from './component/core/interceptors/error.interceptor';
 import { CoreModule } from './component/core/core.module';
-
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoadingInterceptor } from './component/core/interceptors/loading.interceptor';
 @NgModule({
   declarations: [AppComponent],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    { provide:HTTP_INTERCEPTORS,useClass:LoadingInterceptor,multi: true}
   ],
   bootstrap: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
--- src/app/component/core/core.module.ts
+++ src/app/component/core/core.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToastrModule } from 'ngx-toastr';
 import { CoreRoutingModule } from './core-routing.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
+import { NgModule } from '@angular/core';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [NotFoundComponent, ServerErrorComponent],
@@ -15,7 +16,8 @@ import { ServerErrorComponent } from './server-error/server-error.component';
       positionClass: 'toast-bottom-right',
       preventDuplicates: true,
     }),
-
+    NgxSpinnerModule
   ],
+  exports:[NgxSpinnerModule]
 })
 export class CoreModule {}
